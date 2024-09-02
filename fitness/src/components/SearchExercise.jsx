f625d1554fa6b83719a5e8637b37a0833986573b
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-09-02 22:51:43 +0530

    Search Exercise

--- fitness/src/components/SearchExercise.jsx
+++ fitness/src/components/SearchExercise.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchExercise = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async () => {
+    if (search) {
+      // const gymData = await fetchData
+    }
+  };
   return (
-    <div className="flex h-screen flex-col items-center border-2 ">
-      <h1>
-        {" "}
-        Awesome Exercises You <br /> Should Know
-      </h1>
-      <div className="flex  gap-2 w-2/5">
-      <input type="text" placeholder="Search Exersise" className="border-2 rounded-md w-4/5  p-2" />
-      <button className="border-2 w-1/5 rounded-md ">Search</button>
+    <div className="flex h-screen flex-col items-center gap-2 ">
+      <div className="text-4xl flex flex-col justify-center items-center">
+        <h1 className=" "> Awesome Exercises You</h1>
+        <span>Should Know</span>
       </div>
-     
+
+      <div className="flex  gap-2 w-2/5">
+        <input
+          type="text"
+          placeholder="Search Exersise"
+          className="border-2 rounded-md w-4/5 p-2"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+        <button className="border-2 w-1/5 rounded-md " onClick={handleSearch}>
+          Search
+        </button>
+      </div>
     </div>
   );
 };
